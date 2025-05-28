946cad50d53691acedd06712e3744d604b408a53
aaryan-dharmadhikari <56603891+aaryan-dharmadhikari@users.noreply.github.com>
2025-05-28 21:45:29 +0100

    deleting old experiments for linear staging

--- oregano/shared/src/main/scala/oregano/internal/MacroPlayground.scala
+++
@@ -1,9 +0,0 @@
-package oregano.internal
-
-@main def testMatchShenanigans(): Unit =
-    val matcher = testMatch
-    println(matcher(97)) 
-    println(matcher(98)) 
-    println(matcher(99)) 
-    println(matcher(100)) 
-    println(matcher(101)) 
