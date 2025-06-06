e0140d4dcd1f9582a8d2e6662d3ad4c45bf96fd6
aaryan-dharmadhikari <56603891+aaryan-dharmadhikari@users.noreply.github.com>
2025-06-06 19:40:28 +0100

    support for unapplySeq

--- oregano/shared/src/main/scala/oregano/Regex.scala
+++ oregano/shared/src/main/scala/oregano/Regex.scala
@@ -11,7 +11,8 @@ abstract class Regex[Match] {
   def matches(input: CharSequence): Boolean
   def matchesWithCaps(input: CharSequence): Option[Array[Int]]
   def matchesLinear(input: CharSequence): Boolean
-  def unapplySeq(input: CharSequence): Option[Match]
+  def find(input: CharSequence): Boolean 
+  def unapplySeq(input: CharSequence): Option[List[String]]
 }
 
 object Regex {
@@ -22,10 +23,13 @@ object Regex {
     private val pattern = patternResult.pattern
     private val numGroups = patternResult.groupCount
     private val prog = internal.ProgramCompiler.compileRegexp(pattern, numGroups)
+    private val re2Machine = internal.RE2Machine(prog)
     def matches(input: CharSequence): Boolean = compiled.matches(input)
     def matchesWithCaps(input: CharSequence): Option[Array[Int]] = ???
-    def matchesLinear(input: CharSequence): Boolean = ???
-    def matchesBacktrack(input: CharSequence): Boolean = internal.BacktrackingProgMatcher.matches(prog, input )
+    def matchesLinear(input: CharSequence): Boolean = re2Machine.matches(input)
+
+    def find(input: CharSequence): Boolean = ???
+    
     def unapplySeq(input: CharSequence): Option[List[String]] = compiled.unapplySeq(input)
   }
 }
--- oregano/shared/src/main/scala/oregano/internal/macro.scala
+++ oregano/shared/src/main/scala/oregano/internal/macro.scala
@@ -6,99 +6,93 @@
 package oregano.internal
 
 import scala.quoted.*
-import codes.quine.labo.re2s._
+// import codes.quine.labo.re2s._
 
-// TODO: move to utils or something
-def countNodes(expr: Expr[Any])(using Quotes): Int =
-  import quotes.reflect.*
-  // println(expr.show)
-
-  def loop(tree: Tree): Int = 
-    tree match
-      case Inlined(_, _, body) => 1 + loop(body)
-      case Block(stats, expr) => 1 + stats.map(loop).sum + loop(expr)
-      case Apply(fun, args) => 1 + loop(fun) + args.map(loop).sum
-      case TypeApply(fun, args) => 1 + loop(fun) + args.map(loop).sum
-      case Select(qualifier, _) => 1 + loop(qualifier)
-      case Ident(_) => 1
-      case Literal(_) => 1
-      case If(cond, thenp, elsep) => 1 + loop(cond) + loop(thenp) + loop(elsep)
-      case Match(selector, cases) => 1 + loop(selector) + cases.map {
-                                            case CaseDef(_, guard, rhs) =>
-                                              1 + guard.map(loop).getOrElse(0) + loop(rhs)
-                                          }.sum
-      case ValDef(_, _, rhsOpt) => 1 + rhsOpt.map(loop).getOrElse(0)
-      case DefDef(_, _, _, bodyOpt) => 1 + bodyOpt.map(loop).getOrElse(0)
-      case Typed(expr, _) => 1 + loop(expr)
-      case This(_) => 1
-      case New(_) => 1
-      case While(cond, body) => 1 + loop(cond) + loop(body)
-      case Return(expr, _) => 1 + loop(expr)
-      case Assign(lhs, rhs) => 1 + loop(lhs) + loop(rhs)
-      case Repeated(elems, _) => 1 + elems.map(loop).sum
-      case Lambda(_, body) => 1 + loop(body)
-      case Closure(meth, tpeOpt) => 1 + loop(meth) + tpeOpt.map(_ => 1).getOrElse(0)
-      case _: TypeTree => 1
-      case _ => 
-        // This is essentially to flag if I missed something: this function has been grokked from staring at Scala compiler source code!
-        println(s"Unhandled tree type: ${tree.getClass.getSimpleName}")
-        1  // final catch-all for safety
-
-  loop(expr.asTerm)
-
-private inline def selectLinearBehaviour(inline bool: Boolean)(using Quotes): Boolean = ???
-
-private [oregano] def compileMacro(s: String)(using Quotes): Expr[oregano.Regex[?]] = {
-    import quotes.reflect.report
-    parse(s) match
+private[oregano] def compileMacro(
+    s: String
+)(using Quotes): Expr[oregano.Regex[?]] = {
+  import quotes.reflect.report
+  parse(s) match
     case Right(ast) =>
-        // report.info(s"$ast")
-        val patternResult = Pattern.compile(ast)
-        // report.info(s"Parsley AST: ${ast.toString}\nPattern: ${patternResult.pattern}, groupCount: ${patternResult.groupCount}")
-        val p = patternResult.pattern
-        val groupCount = patternResult.groupCount
-        val prog = ProgramCompiler.compileRegexp(p, groupCount)
-        // report.info(s"Prog:\n$prog")
-        val liftedProgExpr = Expr(prog)
-        // println(s"Prog:\n$prog")
+      // report.info(s"$ast")
+      val patternResult = Pattern.compile(ast)
+      // report.info(s"Parsley AST: ${ast.toString}\nPattern: ${patternResult.pattern}, groupCount: ${patternResult.groupCount}")
+      val p = patternResult.pattern
+      val groupCount = patternResult.groupCount
+      val prog = ProgramCompiler.compileRegexp(p, groupCount)
+      // report.info(s"Prog:\n$prog")
+      val liftedProgExpr = Expr(prog)
+      // println(s"Prog:\n$prog")
 
-        // backtracking matcher stuff
-        val backtrackingMatcherExpr: Expr[CharSequence => Option[Array[Int]]] = 
-          if patternResult.flatControlFlow then
-            BacktrackingProgMatcher.genMatcherWithCaps(prog)
-          else
-            CPSMatcher.genMatcherPatternWithCaps(p, groupCount)
-        // useful for debugging: 
-        // val backtrackCPSMatcherExpr = CPSMatcher.genMatcherPatternWithCaps(p, groupCount)
-        // val backtrackProgMatcherExpr = BacktrackingProgMatcher.genMatcherWithCaps(prog)
+      // backtracking matcher stuff
+      val backtrackingMatcherWithCapsExpr
+          : Expr[CharSequence => Option[Array[Int]]] =
+        if patternResult.flatControlFlow then
+          BacktrackingProgMatcher.genMatcherWithCaps(prog)
+        else CPSMatcher.genMatcherPatternWithCaps(p, groupCount)
 
+      val backtrackingMatcherExpr: Expr[CharSequence => Boolean] =
+        if patternResult.flatControlFlow then
+          BacktrackingProgMatcher.genMatcher(prog)
+        else CPSMatcher.genMatcherPattern(p)
 
-        // Linear matching stuff
-        val linearMatcherExpr = StagedMachine.generateStepLoop(prog)
-        val willBeLinearStaged = countNodes(linearMatcherExpr) < 4500
-        if (!willBeLinearStaged) then report.warning(s"Matcher for $s is too large, will not be linear staged")
+      val backtrackingFinderExpr: Expr[CharSequence => Boolean] =
+        if patternResult.flatControlFlow then
+          BacktrackingProgMatcher.genFind(prog)
+        else CPSMatcher.genFinderPattern(p, groupCount)
+      // useful for debugging:
+      // val backtrackCPSMatcherExpr = CPSMatcher.genMatcherPattern(p)
+      // val backtrackingCPSMatcherWithCaps = CPSMatcher.genMatcherPatternWithCaps(p, groupCount)
+      // val backtrackProgMatcherExpr = BacktrackingProgMatcher.genMatcher(prog)
+      // val backtrackProgMatcherWithCaps = BacktrackingProgMatcher.genMatcherWithCaps(prog)
 
-        // for testing:
-        // val nodeCount = countNodes(linearMatcherExpr)
-        // println(s"regex: $s counted nodes: $nodeCount, numInst: ${prog.numInst}")
-        '{
-            new oregano.Regex[List[String]] {
-                val regex = ${Expr(s)}.r
-                val re2 = ${Expr(s)}.r2
-                val prog = $liftedProgExpr
-                val re2Machine = RE2Machine(prog)
+      // Linear matching stuff
+      var linearMatcherExpr = StagedMachine.generateStepLoop(prog)
+      val willBeLinearStaged =
+        Utils.countNodes(linearMatcherExpr) < 4500 // heuristic
+      if (!willBeLinearStaged) then
+        report.warning(
+          s"Matcher for $s is too large, will not be linear staged"
+        )
+        // set linearMatcherExpr to just use runtime
+        linearMatcherExpr = '{ (m: RE2Machine, input: CharSequence) =>
+          m.matches(input)
+        }
 
-                def matches(input: CharSequence): Boolean = $backtrackingMatcherExpr(input).isDefined
-                def matchesWithCaps(input: CharSequence): Option[Array[Int]] = $backtrackingMatcherExpr(input)
-                def matchesLinear(input: CharSequence): Boolean = 
-                    // This should be evaluated at compile time: does not explode for large regexes
-                    if ${Expr(willBeLinearStaged)} then 
-                        $linearMatcherExpr(re2Machine, input) 
-                    else 
-                        re2Machine.matches(input)
+      // for testing:
+      // val nodeCount = Utils.countNodes(backtrackProgMatcherExpr)
+      // println(s"regex: $s counted nodes: $nodeCount, numInst: ${prog.numInst}")
+      // val nodeCountWithCaps = Utils.countNodes(backtrackProgMatcherWithCaps)
+      // println(s"regex: $s counted nodes: $nodeCountWithCaps, numInst: ${prog.numInst}")
+      '{
+        new oregano.Regex[List[String]] {
+          val regex = ${ Expr(s) }.r
+          // val re2 = ${Expr(s)}.r2
+          val prog = $liftedProgExpr
+          val re2Machine = RE2Machine(prog)
 
-                def unapplySeq(input: CharSequence): Option[List[String]] = regex.unapplySeq(input)
+          def matches(input: CharSequence): Boolean =
+            $backtrackingMatcherExpr(input)
+          // def matches(input: CharSequence): Boolean = regex.matches(input)
+          def matchesWithCaps(input: CharSequence): Option[Array[Int]] =
+            $backtrackingMatcherWithCapsExpr(input)
+          def matchesLinear(input: CharSequence): Boolean =
+            $linearMatcherExpr(re2Machine, input)
+          def find(input: CharSequence): Boolean =
+            $backtrackingFinderExpr(input)
+          // def matchesLinear(input: CharSequence): Boolean = re2Machine.matches(input)
+          def unapplySeq(input: CharSequence): Option[List[String]] =
+            matchesWithCaps(input).map { caps =>
+              caps
+                .drop(2)
+                .sliding(2, 2)
+                .collect {
+                  case Array(start, end) if start != -1 && end != -1 =>
+                    input.subSequence(start, end).toString
+                }
+                .toList
             }
         }
+      }
     case Left(err) => report.errorAndAbort(err)
 }
